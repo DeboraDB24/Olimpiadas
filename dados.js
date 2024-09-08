05605b1363dc5330a1c976d838097682667018ca
DeboraDB24 <deboradominguesborges@gmail.com>
2024-09-07 22:03:24 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+    titulo : "Rayssa Leal",
+    descricao : "Rayssa Leal, conhecida carinhosamente como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento precoce e carisma contagiante. Aos poucos anos, ela se tornou um fenômeno nas redes sociais e, em seguida, uma medalhista olímpica, conquistando o coração dos brasileiros. Sua trajetória inspiradora e suas manobras incríveis a tornaram um ícone do esporte e um exemplo para jovens atletas.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
+},
+{
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que se destacou nas Olimpíadas de Paris 2024, conquistando a medalha de ouro. Com sua técnica impecável e determinação, ela se tornou um dos principais nomes do judô brasileiro. Sua trajetória é marcada por superação e dedicação, inspirando jovens atletas a perseguirem seus sonhos.",
+    link: "https://en.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judô judo judoca judoka"
+},
+{
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e beleza. Nas Olimpíadas de Paris 2024, ela se tornou uma das grandes estrelas, conquistando diversas medalhas. Sua força, determinação e talento a tornaram um ícone da ginástica mundial e um orgulho para o Brasil.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginástica ginastica ouro"
+}
+];
+
